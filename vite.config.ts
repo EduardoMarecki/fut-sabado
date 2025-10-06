40c3e48fe66a19bb1b55c56872067b7f15ab978c
EduardoMarecki <141771543+EduardoMarecki@users.noreply.github.com>
2025-10-06 00:02:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+/*export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
@@ -15,4 +15,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}));*/
+
+export default defineConfig({
+  base: '/fut-sabado/',
+  ...
+})
